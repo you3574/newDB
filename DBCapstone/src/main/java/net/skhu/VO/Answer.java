6e4d0123ff28a0cdbabf750c7f603c5882e3f0d6
chuchu <miho7724@gmail.com>
2018-11-18 02:23:42 +0900

    QnA 게시판 일단 완성!(내글보기 글작성가능)

---
+++ DBCapstone/src/main/java/net/skhu/VO/Answer.java
@@ -0,0 +1,44 @@
+package net.skhu.VO;
+
+import java.util.Date;
+
+public class Answer {
+
+	int id;
+	int adminId;
+	String message;
+	Date time;
+	int articleId;
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getAdminId() {
+		return adminId;
+	}
+	public void setAdminId(int adminId) {
+		this.adminId = adminId;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public Date getTime() {
+		return time;
+	}
+	public void setTime(Date time) {
+		this.time = time;
+	}
+	public int getArticleId() {
+		return articleId;
+	}
+	public void setArticleId(int articleId) {
+		this.articleId = articleId;
+	}
+
+}
